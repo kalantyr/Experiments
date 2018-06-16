0cd6d4fae9d54b0d8ea6135b933ab10d4463ddcb
kalantyr <kalantyr@yandex.ru>
2018-06-16 13:10:04 +0300

    1

---
+++ ElasticSearchExperiment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace ElasticSearchExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sinkOptions = new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6
+            };
+            var loggerConfig = new LoggerConfiguration()
+                .WriteTo
+                .Elasticsearch(sinkOptions);
+
+            Log.Logger = loggerConfig.CreateLogger();
+
+            Log.Information("Hello, Serilog!");
+
+            Log.CloseAndFlush();
+        }
+    }
+}

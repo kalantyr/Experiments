be46fb0cac142058f7b7a4e9b1259973a6fe96ea
Sergey Kalantyr <kalantyr@vtb.ru>
2018-07-19 11:48:19 +0300

    Queue

---
+++ Queue/QueueReceiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Messaging;
+
+namespace QueueReceiver
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var msgQ = new MessageQueue(@".\Private$\test")
+            {
+                Formatter = new XmlMessageFormatter(new[] {typeof(string)})
+            };
+
+            while (true)
+            {
+                var message = msgQ.Receive();
+                Console.WriteLine(message.Body);
+            }
+        }
+    }
+}
---
+++ Queue/QueueSender/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Messaging;
+
+namespace QueueSender
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var msgQ = new MessageQueue(@".\Private$\test");
+
+            while (true)
+            {
+                var msgBody = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(msgBody))
+                    break;
+
+                Message msg = new Message
+                {
+                    Body = msgBody
+                };
+                msgQ.Send(msg);
+            }
+        }
+    }
+}

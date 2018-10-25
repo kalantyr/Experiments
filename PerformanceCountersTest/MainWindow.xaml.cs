47728a7c7d9b809268964a72a9995cf7676688be
kalantyr <kalantyr.work@gmail.com>
2018-10-25 17:10:55 +0300

    1

--- PerformanceCountersTest/MainWindow.xaml.cs
+++ PerformanceCountersTest/MainWindow.xaml.cs
@@ -1,44 +1,25 @@
 ﻿using System.Diagnostics;
-using System.Linq;
 using System.Windows;
+using VTB.Portal.ExtendedLogging;
 
 namespace PerformanceCountersTest
 {
     public partial class MainWindow
     {
-        private const string CATEGORY_NAME = "_Temp";
-        private const string CATEGORY_HELP = "test";
-        private const string COUNTER_NAME = "T_1";
+	    private const string COUNTER_NAME = "T_1";
+		private const string COUNTER2_NAME = "T_1_";
         private static readonly PerformanceCounterCategory _performanceCounterCategory;
 
-        static MainWindow()
+	    private static readonly IPerformanceCollector _performanceCollector;
+
+		static MainWindow()
         {
             var counterCreationDatas = new[]
             {
-                new CounterCreationData(COUNTER_NAME, "Help", PerformanceCounterType.NumberOfItems64)
-            };
-
-            var category = PerformanceCounterCategory.GetCategories().FirstOrDefault(cat => cat.CategoryName == CATEGORY_NAME);
-            if (category != null)
-            {
-                foreach (var creationData in counterCreationDatas)
-                    if (!category.CounterExists(creationData.CounterName))
-                    {
-                        PerformanceCounterCategory.Delete(CATEGORY_NAME);
-
-                        var counterDataCollection = new CounterCreationDataCollection();
-                        counterDataCollection.AddRange(counterCreationDatas);
-                        _performanceCounterCategory = PerformanceCounterCategory.Create(CATEGORY_NAME, CATEGORY_HELP, PerformanceCounterCategoryType.MultiInstance, counterDataCollection);
-                        return;
-                    }
-                _performanceCounterCategory = category;
-            }
-            else
-            {
-                var counterDataCollection = new CounterCreationDataCollection();
-                counterDataCollection.AddRange(counterCreationDatas);
-                _performanceCounterCategory = PerformanceCounterCategory.Create(CATEGORY_NAME, CATEGORY_HELP, PerformanceCounterCategoryType.MultiInstance, counterDataCollection);
-            }
+	            new CounterCreationData(COUNTER_NAME, "Help", PerformanceCounterType.NumberOfItems64),
+	            new CounterCreationData(COUNTER2_NAME, "Help", PerformanceCounterType.RateOfCountsPerSecond64)
+			};
+	        _performanceCollector = PerformanceCollector.Create("_Temp1", "test", counterCreationDatas);
         }
 
         public MainWindow()
@@ -48,15 +29,10 @@ namespace PerformanceCountersTest
 
         private void RangeBase_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            using (var counter = new PerformanceCounter(_performanceCounterCategory.CategoryName, COUNTER_NAME, GetInstanceName(), false))
-                counter.RawValue = (long)e.NewValue;
-        }
-
-        private static string GetInstanceName()
-        {
-            var process = Process.GetCurrentProcess();
-            return $"{process.ProcessName}_{process.Id}";
-            //return Process.GetCurrentProcess().MainModule.FileName;
-        }
-    }
+	        var newValue = (long)e.NewValue;
+	        _performanceCollector.SetValue(COUNTER_NAME, newValue);
+			for (var i = 0; i < newValue; i++)
+				_performanceCollector.Increment(COUNTER2_NAME);
+		}
+	}
 }

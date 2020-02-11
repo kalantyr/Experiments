4e2f0eb946e2a86ad6352940d76de1db1eeff73c
kalantyr <kalantyr@yandex.ru>
2020-02-11 08:15:07 +0300

    healthCheck

---
+++ HealthCheckTest/WebApplication1/ExampleHealthCheck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WebApplication1
+{
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        internal static readonly Random R = new Random();
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            switch (R.Next(3))
+            {
+                case 1:
+                    return Task.FromResult(
+                        HealthCheckResult.Degraded("Чёт херовато"));
+                
+                case 2:
+                    return Task.FromResult(
+                        HealthCheckResult.Unhealthy("Аларм!",
+                            new ApplicationException("Связь с БД потеряна"),
+                            new Dictionary<string, object> {{ "ERROR", "ORA-666" }}));
+
+                default:
+                    return Task.FromResult(
+                        HealthCheckResult.Healthy("Всё норм"));
+            }
+        }
+    }
+
+    public class ExampleHealthCheck2 : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            switch (ExampleHealthCheck.R.Next(3))
+            {
+                case 1:
+                    return Task.FromResult(
+                        HealthCheckResult.Degraded("Чёт херовато 2"));
+
+                case 2:
+                    return Task.FromResult(
+                        HealthCheckResult.Unhealthy("Аларм! 2",
+                            new ApplicationException("Связь с БД потеряна"),
+                            new Dictionary<string, object> { { "ERROR", "ORA-666" } }));
+
+                default:
+                    return Task.FromResult(
+                        HealthCheckResult.Healthy("Всё норм 2"));
+            }
+        }
+    }
+}
---
+++ HealthCheckTest/WebApplication1/Startup.cs
@@ -0,0 +1,81 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHealthChecks()
+                .AddCheck<ExampleHealthCheck>("example_health_check")
+                .AddCheck<ExampleHealthCheck2>("example_health_check_2");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions
+                {
+                    ResultStatusCodes =
+                    {
+                        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                        [HealthStatus.Degraded] = StatusCodes.Status429TooManyRequests,
+                        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                    },
+                    ResponseWriter = WriteResponse
+                });
+            });
+        }
+
+        private static Task WriteResponse(HttpContext context, HealthReport result)
+        {
+            // https://datatracker.ietf.org/doc/draft-inadarei-api-health-check/?include_text=1
+
+            context.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+
+            return context.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
+        }
+    }
+}
